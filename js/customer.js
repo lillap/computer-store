7dddf9a6014142266e5f00943ea032808c0d6b6f
NORDIC\pkarim <paria.karim@gmail.com>
2020-11-05 20:09:34 +0100

    feat: bank requirement logic completed

--- js/customer.js
+++ js/customer.js
@@ -1,23 +1,56 @@
-let customers = [
+let customer = 
     {
         firstName:"Ella",
         lastName: "Lively",
         accountBalance: 2000, 
-    }, 
+        noOfLoans: 0,
+        noOfPurchasedLaptops: 0, 
+    };
 
-    {
-        firstName:"Marion",
-        lastName: "Cotillard",
-        accountBalance: 35000, 
-    },
-    {
-        firstName:"Karen",
-        lastName: "Johnson",
-        accountBalance: 350, 
-    },
+function renderCustomer(){
+    document.getElementById("customerName").innerText = `${customer.firstName} ${customer.lastName}`
+    document.getElementById("customerAccountBalance").innerText = ` ${customer.accountBalance} SEK.`
+}
 
-];
 
-document.getElementById("customerName").innerText = `${customers[0].firstName} ${customers[0].lastName}`
+let loanForm = document.getElementById("loanForm");
+let getLoanBtn = document.getElementById("getLoanBtn");
+let loanInput;
+let submitFormBtn;
+let errorMessage = document.getElementById("loanMessage");
 
-document.getElementById("customerAccountBalance").innerText = ` ${customers[0].accountBalance} SEK.`
+function renderLoanForm(){
+    loanForm.innerHTML = `
+    <input type="text" placeholder="Amount to loan" id="loanInput"></input>
+    <button class="btn btn-sm" id="submitFormBtn">Submit</button>`
+    submitFormBtn = document.getElementById("submitFormBtn");
+    loanInput = document.getElementById("loanInput");
+
+    submitFormBtn.addEventListener("click", (event )=> {
+        let requestedLoan = parseInt(loanInput.value);
+        console.log(requestedLoan);
+ 
+        if(customer.noOfLoans <= customer.noOfPurchasedLaptops ){
+           if(requestedLoan <= customer.accountBalance* 2){ customer.accountBalance += requestedLoan;
+                renderCustomer();
+                customer.noOfLoans++;
+                loanForm.innerHTML ="";
+            } else {
+                errorMessage.innerText = "Amount to loan is not allowed, try a lesser amount.";
+            }
+        } else {
+             errorMessage.innerText = "Loan not permitted before a purchase with the latest loan  has  been made. "     
+        }
+    })
+}
+
+getLoanBtn.addEventListener("click",(event ) => {
+    event.preventDefault();
+    renderLoanForm();
+})
+
+renderCustomer();
+
+
+
+
